411e924cdbe8874a3249b0a32984f65e594722f2
jykdavid <jykdavid.kim@gmail.com>
2022-06-19 01:28:12 -0400

    Added ClearRow, MoveRowDown & ClearFullRows functions

--- Tetris/Tetris/GameGrid.cs
+++ Tetris/Tetris/GameGrid.cs
@@ -2,6 +2,7 @@
 {
     public class GameGrid
     {
+        // grid starts at the top left (0, 0)
         private readonly int[,] grid;
         public int Rows { get; }
         public int Columns { get; }
@@ -13,6 +14,7 @@
             set => grid[r, c] = value;
         }
 
+        // GameGrid Constructor
         public GameGrid(int rows, int columns)
         {
             Rows = rows;
@@ -56,6 +58,44 @@
                 }
             }
             return true;
+        }
+
+        // Clears a row
+        private void ClearRow(int r)
+        {
+            for (int c = 0; c < Columns; c++)
+            {
+                grid[r, c] = 0;
+            }
+        }
+
+        // Move row down due to previous row(s) cleared
+        private void MoveRowDown(int r, int numRows)
+        {
+            for (int c = 0; c < Columns; c++)
+            {
+                grid[r + numRows, c] = grid[r, c];
+                grid[r, c] = 0;
+            }
+        }
+
+        // Clear full rows (starts from the bottom row)
+        public int ClearFullRows()
+        {
+            int cleared = 0;
+            for (int r = Rows - 1; r >= 0; r--)
+            {
+                if (IsRowFull(r))
+                {
+                    ClearRow(r);
+                    cleared++;
+                }
+                else if (cleared > 0)
+                {
+                    MoveRowDown(r, cleared);
+                }
+            }
+            return cleared;
         }
     }
 }
